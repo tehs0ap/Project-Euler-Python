47566cc98cf49827e9d65d1fc8deefb8a785a06b
Martin Viens <marty.viens@gmail.com>
2012-12-17 16:39:27 -0500

    Added Problem 22

--- Solutions/Problems_20-29/Problem_22.py
+++ Solutions/Problems_20-29/Problem_22.py
@@ -4,11 +4,13 @@ Created on 2012-12-06
 @author: Marty
 '''
 '''
-Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, 
-begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by its 
-alphabetical position in the list to obtain a name score.
+Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over 
+five-thousand first names, begin by sorting it into alphabetical order. Then working out the 
+alphabetical value for each name, multiply this value by its alphabetical position in the list to 
+obtain a name score.
 
-For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. 
+For example, when the list is sorted into alphabetical order, COLIN, which is worth 
+3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. 
 So, COLIN would obtain a score of 938  53 = 49714.
 
 What is the total of all the name scores in the file?
@@ -17,6 +19,13 @@ import time
 
 startTime = time.time()
 total = 0
-    
+
+file = open('Problem_22.txt', 'r+')
+nameList = file.readline().replace('"','').split(",")
+nameList.sort()
+             
+for i in range(0, len(nameList)):    
+  total += reduce(lambda x, y: x + y , [ord(c)-96 for c in nameList[i].lower()])*(i+1)
+   
 print total
 print "Time Elapsed: " + str(time.time() - startTime)
